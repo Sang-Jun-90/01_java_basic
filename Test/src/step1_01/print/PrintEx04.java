9052c13bb5eb76f6b07fa76d5bcddd9aa3e0a3cf
19시_웹개발_LSJ <19__LSJ@DESKTOP-VD237BJ>
2021-03-26 21:43:35 +0900

    1

---
+++ Test/src/step1_01/print/PrintEx04.java
@@ -0,0 +1,16 @@
+package step1_01.print;
+
+import java.util.Date;
+
+public class PrintEx04 {
+
+	public static void main(String[] args) {
+	
+		new Date();
+		System.out.println(new Date());
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+	}
+
+}
